33c85e713839b7a5b72612d95d2bc49fe26106c0
Maxwell Singer <maxwellsinger@Maxwells-Air.med.yale.internal>
2024-09-15 18:35:06 -0400

    fix accordion

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -3,11 +3,10 @@ import { Analytics } from '@vercel/analytics/react';
 
 const DynamicLinks = dynamic(() => import('../../components/DynamicLinks'), { ssr: false })
 
-
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto flex flex-col space-y-4">
-      <header className="container sticky top-0 z-40 bg-white">
+    <div className="mx-auto flex flex-col min-h-screen">
+      <header className="sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6">
             <a href="#" className="hover:text-slate-600 cursor-pointer">
@@ -16,12 +15,10 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
           </nav>
         </div>
       </header>
-      <div>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
-      </div>
-      <footer className="m-auto p-4">
+      <main className="flex-grow overflow-auto">
+        {children}
+      </main>
+      <footer className="mt-auto p-4 text-center">
         Powered by GPT-4o and LangChainAI | Built by <DynamicLinks />
       </footer>
     </div>
